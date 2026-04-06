31b26f2c5baea16a5f96c8bdd10d4fdbb0d0ecec
kaykay0201 <ledinhhoangkhai@gmail.com>
2026-04-06 15:16:49 +0700

    fix: build.rs VERSION matches release tag v0.1.1

--- clawser-sys/build.rs
+++ clawser-sys/build.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::io;
 use std::path::PathBuf;
 
-const VERSION: &str = "0.1.0";
+const VERSION: &str = "0.1.1";
 const GITHUB_REPO: &str = "kaykay0201/clawser-fetch-engine";
 
 fn main() {
